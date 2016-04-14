5c525790f8f7af289f24acfb40e26c8c0168a2eb
bubkoo <bubkoo.wy@gmail.com>
2016-04-14 13:57:01 +0800

    test spec

--- test/spec/index.js
+++ test/spec/index.js
@@ -10,12 +10,20 @@ describe('pick-some: ', function () {
   it('common', function () {
 
     expect(pickItems([1, 2, 3])).to.have.length.most(3);
+
     expect(pickItems([1, 2, 3], 1)).to.have.length(1);
+
     expect(pickItems([1, 2, 3], { count: 2 })).to.have.length(2);
-    expect(pickItems([1, 2, 3], { shuffled: true })).to.have.length(2);
+
+    expect(pickItems([1, 2, 3], {
+      count: 2,
+      shuffled: true
+    })).to.have.length(2);
+
     expect([1, 2, 3]).to.include.members(pickItems([1, 2, 3]));
 
     expect(pickItems([1, 2, 3], 0)).to.eql([]);
+
     expect(pickItems([], 2)).to.eql([]);
 
 
